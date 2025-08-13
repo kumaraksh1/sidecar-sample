bf1a1f436f95022e412a703057829fdd87994c59
Akshay Kumar <kumaraksh@microsoft.com>
2025-08-14 01:59:13 +0530

    push changes for container

--- hello-app/app.py
+++ hello-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello from Hello App!, testing sidecar gha | final test before release"})
+    return jsonify({"message": "Hello from Hello App!, testing sidecar gha | Testing for final workflow"})
 
 
 @app.route('/read-file', methods=['GET'])
